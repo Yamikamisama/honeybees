70d537ff9eabeb02a2122ea1728494bb3b999fda
Robert Yang <robertyang@Rob-Yangs-MacBook-Pro.local>
2015-10-24 18:13:14 -0400

    Create placeHolderList module for cleaner referencing.

---
+++ Swift/src/js/placeHolderList.js
@@ -0,0 +1,49 @@
+/*
+
+This list provides a dummy array of JSON sent from the iOS companion app.
+Need something to work with.
+
+ */
+
+var placeHolderList = [{
+  "id": 1,
+  "title": "Study Pebble",
+  "subtitle": "Get it done",
+  "description": "I want to buy a something and that thing may or may not be this thing",
+  "location": {
+    "name": "Duane Reade",
+    "address": "123 East 123 St., New York, NY 10010",
+    "lat": -34.2134,
+    "lon": 72.1234
+  },
+  "icon": "images/bunny.png"
+}, {
+  "id": 2,
+  "title": "Study Pebble",
+  "subtitle": "Get it done",
+  "description": "I want to buy a something and that thing may or may not be this thing",
+  "location": {
+    "name": "Duane Reade",
+    "address": "123 East 123 St., New York, NY 10010",
+    "lat": -34.2134,
+    "lon": 72.1234
+  },
+  "icon": "images/bunny.png"
+},
+{
+  "id": 3,
+  "title": "Study Pebble",
+  "subtitle": "Get it done",
+  "description": "I want to buy a something and that thing may or may not be this thing",
+  "location": {
+    "name": "Duane Reade",
+    "address": "123 East 123 St., New York, NY 10010",
+    "lat": -34.2134,
+    "lon": 72.1234
+  },
+  "icon": "images/bunny.png"
+}];
+
+if (typeof module !== 'undefined') {
+  module.exports = Vector2;
+}
